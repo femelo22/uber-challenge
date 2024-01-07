7683abb78709b1be5c543056d7c9ebdf2ede08b5
femelo22 <luiz123jfmg@gmail.com>
2024-01-07 15:35:15 -0300

    chore: add region aws ses

--- src/main/java/com/lfmelo/application/EmailSenderService.java
+++ src/main/java/com/lfmelo/application/EmailSenderService.java
@@ -3,7 +3,9 @@ package com.lfmelo.application;
 import com.lfmelo.adapters.EmailSenderGateway;
 import com.lfmelo.core.EmailSenderUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailSenderService implements EmailSenderUseCase {
 
     private final EmailSenderGateway emailSenderGateway;
